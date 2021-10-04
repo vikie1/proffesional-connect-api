6a66c851d0d666c2c7b077b756e50abef16dc259
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-10-04 11:11:15 +0300

    initial commit

---
+++ src/main/java/io/github/vikie1/projectapi/ProjectApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.vikie1.projectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vikie1/projectapi/ProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vikie1.projectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
