604bdd08652d2a7e012ebc78549736bdde85f996
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-10-19 18:54:52 +0300

    mhh

---
+++ src/main/java/io/github/vikie1/projectapi/configuration/security/AccessControl.java
@@ -0,0 +1,31 @@
+package io.github.vikie1.projectapi.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration @EnableWebSecurity
+public class AccessControl extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/admin/**", "/actuator/**").hasRole("ADMIN")
+                .antMatchers("/api/**").authenticated();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+            }
+        };
+    }
+}
